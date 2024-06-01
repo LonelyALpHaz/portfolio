affb8a4335ea24d826ecffb92504e747a98992a0
LonelyALpHaz <dantas.arthur.contact@gmail.com>
2024-06-01 17:34:36 -0300

    Minor improvments

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Click and Copy email function
+
 var span = document.getElementById("email");
 
 span.onclick = function() {
@@ -20,5 +22,24 @@ span.addEventListener("copy", function(event) {
     if (event.clipboardData) {
         event.clipboardData.setData("text/plain", span.textContent);
         console.log(event.clipboardData.getData("text"));
-    }
-});
+    };
+})
+
+// Go to top button function
+
+var mybutton = document.getElementById("topbtn");
+
+window.onscroll = function() {scroolFunction()};
+
+function scroolFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    };
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
